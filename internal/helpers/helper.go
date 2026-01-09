5fc361e4fe44d9aa2c22b99289a4d729b1724e07
VslVictor7 <vic.collfac358@outlook.com>
2026-01-09 20:15:55 -0300

    feat: error handling

--- internal/helpers/helper.go
+++ internal/helpers/helper.go
@@ -2,13 +2,17 @@ package helpers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Printf("json encode error: %v", err)
+	}
 }
 
 func BaseURL(r *http.Request) string {
